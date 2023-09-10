28a92a26b8a154d021283e1e364228f6f694d993
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-09-10 15:17:12 +0200

    move some code outside component

--- src/DateHeader.jsx
+++ src/DateHeader.jsx
@@ -1,27 +1,29 @@
+const dayNames = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 export default function DateHeader() {
-  const dayNames = [
-    "sunday",
-    "monday",
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "saturday",
-  ];
-  const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
   const date = new Date();
   return (
     <h1 className='date-header'>
