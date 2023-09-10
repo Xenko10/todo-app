552e6125ce253e286d0d2dba94b2a5ff52f47c0d
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-09-10 15:15:07 +0200

    destructure props in Item component

--- src/Item.jsx
+++ src/Item.jsx
@@ -2,22 +2,22 @@ import {number, string, func} from 'prop-types'
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 
-export default function Item(props) {
+export default function Item({time,deleteTask,id,task}) {
   function handleClick() {
-    props.deleteTask(props.id);
+    deleteTask(id);
   }
 
   return (
     <div className='item'>
-      <h2 className='item-time'>{props.time}</h2>
+      <h2 className='item-time'>{time}</h2>
       <div className='delete-button'>
-        {props.time !== "now" && (
+        {time !== "now" && (
           <IconButton aria-label='delete' onClick={handleClick}>
             <DeleteIcon />
           </IconButton>
         )}
       </div>
-      <h2 className='item-task'>{props.task}</h2>
+      <h2 className='item-task'>{task}</h2>
       <hr />
     </div>
   );
