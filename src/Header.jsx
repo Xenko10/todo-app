ec59b40db6ce0dd9d495f663824bc0a0daaa9c7e
Xenko10 <kubinios_2002@o2.pl>
2023-11-27 22:07:20 +0100

    code refactor

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,8 +2,10 @@ import EventNoteIcon from "@mui/icons-material/EventNote";
 
 export default function Header() {
   return (
-    <h1 className='logo'>
-      <EventNoteIcon fontSize='large' /> Todo app
-    </h1>
+    <div className='header'>
+      <h1 className='logo'>
+        <EventNoteIcon fontSize='large' /> Todo app
+      </h1>
+    </div>
   );
 }
